9de3c76623afbed39bd0679e1b1232366b0aa44e
Ben Lurye <69798922+benlurye@users.noreply.github.com>
2021-02-19 15:27:31 -0500

    add js load, fix studio contact link

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+    $(".image-container-1").hide().delay(1000).fadeIn(1500, function () {
     
+    });
+
+    $(".image-container-2").hide().delay(1250).fadeIn(1500, function () {
+    
+    });
+
+    $(".image-container-3").hide().delay(1500).fadeIn(1500, function () {
+    
+    });
 
 });
